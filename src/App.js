56e489d1c01822986af3559a5f2ac39a6ea6773b
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-28 17:43:09 +0200

    Getting data from state & display it, (Connecting to app)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  // Getting acces to whole state
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
   return (
-    <div>Hi</div>
+    <>
+
+      <h1>Counter {counter}</h1>
+
+      <button>+</button>
+      <button>-</button>
+
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
+      
+    </>
   )
 }
 
