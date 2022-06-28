ac009d155c9939ff04005efb8bfbcc4f981c6ef7
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-28 18:05:16 +0200

    I have used use Selector and useDispatch hooks to manage state with onClick functions into buttons to change te states by calling actions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch} from 'react-redux'
+
+// actions
+import { increment, decrement } from './actions'
+
 
 const App = () => {
   // Getting acces to whole state
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <>
 
       <h1>Counter {counter}</h1>
 
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
 
       {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
       
