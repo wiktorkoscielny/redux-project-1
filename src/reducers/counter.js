4da30a481eb3757a854d7806f6dfd2de29a8ab40
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-28 17:26:57 +0200

    DONT FORGET TO ADD A DEFAULT CASE

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,10 +1,13 @@
-const counterReducer = (state =0, action) {
+const counterReducer = (state = 0, action) => {
     switch(action.type) {
         case 'INCREMENT':
             return state + 1;
 
         case 'DECREMENT': 
             return state -1;
+
+        default:
+            return state; // Don't ever forget to add a Default case
     }
-}
+};
 export default counterReducer;
